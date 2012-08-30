9f4288da7c19c374e3c4fbdf7f836d12fec2f44f
Raymond Liu <raymond.liu@intel.com>
2012-08-30 09:33:01 +0800

    tune create_test_table_dot.hive

--- hive/build/cplibrary.sh
+++ hive/build/cplibrary.sh
@@ -7,5 +7,6 @@ HBASEDIR=~/cloud/built/hbase-0.94.0
 cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/hadoopcore/hadoop-0.20.1/lib
 cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/hadoopcore/hadoop-1.0.3/lib
 
+cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/dist/lib
 cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/ivy/lib/default
 cp ~/.m2/repository/com/yammer/metrics/metrics-core/2.1.2/metrics-core-2.1.2.jar build/ivy/lib/default/
