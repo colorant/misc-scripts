908ccd666d92ee5f59c292dc361b1b0fe9a5a2ec
Raymond Liu <raymond.liu@intel.com>
2012-09-03 17:33:53 +0800

    update cplibrary.sh

--- hive/build/cplibrary.sh
+++ hive/build/cplibrary.sh
@@ -9,4 +9,8 @@ cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/hadoopcore/hadoop-1.0.3/lib
 
 cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/dist/lib
 cp $HBASEDIR/lib/protobuf-java-2.4.0a.jar build/ivy/lib/default
+
+cp $HBASEDIR/lib/jettison-1.1.jar build/hadoopcore/hadoop-0.20.1/lib
+cp $HBASEDIR/lib/jettison-1.1.jar build/hadoopcore/hadoop-1.0.3/lib
+
 cp ~/.m2/repository/com/yammer/metrics/metrics-core/2.1.2/metrics-core-2.1.2.jar build/ivy/lib/default/
