dee565538198f4410c13712aa64115ecab67bd45
Raymond Liu <raymond.liu@intel.com>
2013-01-11 09:26:13 +0800

    add preparedisk for hadoop

---
+++ hadoop/preparedisk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#need to be done by root
+#prepare disk dir for hadoop
+
+mydisks="/mnt/DP_disk1 /mnt/DP_disk2 /mnt/DP_disk3 /mnt/DP_disk4"
+mydir=raymond
+
+for x in $mydisks
+do
+dirpath=$x/$mydir
+mkdir $dirpath
+mkdir $dirpath/hdfs
+mkdir $dirpath/hdfs/data
+mkdir $dirpath/hdfs/name
+mkdir $dirpath/hdfs/mapred
+mkdir $dirpath/hdfs/namesecondary
+chown raymond:raymond $dirpath -R
+done
+
