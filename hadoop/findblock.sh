a6b8fc01574e3ffc6cffbf863b5c413715224d10
Raymond Liu <raymond.liu@intel.com>
2013-01-19 18:36:34 +0800

    update findblock scripts

--- hadoop/findblock.sh
+++ hadoop/findblock.sh
@@ -4,23 +4,29 @@
 
 mydisks="/mnt/DP_disk1 /mnt/DP_disk2 /mnt/DP_disk3 /mnt/DP_disk4"
 hdfspath="raymond/hdfs/data/current/"
+tmpfile=averyimpossiblenotexistfile
+
 
 hadoop=/home/raymond/hadoop/bin/hadoop
 file=$2
 host=$1
+found=0
+
+rm $tmpfile -rf
+for disk in $mydisks
+do
+  ssh $host "find $disk/$hdfspath -name '*meta'" >> $tmpfile
+done
 
 blocks=`$hadoop fsck $file -files -blocks -locations 2>/dev/null | grep "blk_" | awk '{print $2}'`
 
 for block in $blocks
 do
-  for disk in $mydisks
-  do
-    result=`ssh $host "find $disk/$hdfspath -name '*$block*'"`
+    result=`grep "$block*" $tmpfile | sed 's/\// /g' | awk '{print $2}'`
     if [ "a"$result != "a" ]
     then
-      location=`echo $result | sed 's/\// /g' | awk '{print $2}'`
-      echo $block ":" $location
-      break
+      echo $block ":" $result
+    else
+      echo $block ": Not Found"
     fi
-  done
 done
